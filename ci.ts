3ada3e2d5470db4cc564253ac811b38b81ca3cb2
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2023-10-21 17:52:53 +0000

    update ci.ts

--- ci.ts
+++ ci.ts
@@ -1,10 +1,3 @@
-import Client, { connect } from "https://sdk.fluentci.io/v0.1.9/mod.ts";
-import { deploy } from "https://pkg.fluentci.io/cloudflare_pipeline@v0.5.3/mod.ts";
+import { deploy } from "https://pkg.fluentci.io/cloudflare_pipeline@v0.6.0/mod.ts";
 
-function pipeline(src = ".") {
-  connect(async (client: Client) => {
-    await deploy(client, src);
-  });
-}
-
-pipeline();
+await deploy(".");
