aae97fc88a0e060f52ebd4567dd5697d40d5c712
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2023-11-12 20:31:16 +0000

    update README.md

--- ci.ts
+++ ci.ts
@@ -1,3 +1,3 @@
-import { deploy } from "https://pkg.fluentci.io/cloudflare_pipeline@v0.6.3/mod.ts";
+import { deploy } from "https://pkg.fluentci.io/cloudflare_pipeline@v0.6.5/mod.ts";
 
 await deploy(".");
