07da861bc019eabfce0c00d6d4e43ccdbd8f6926
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2024-04-02 07:28:40 +0000

    feat: add wasm plugin

---
+++ plugin/src/lib.rs
@@ -0,0 +1,37 @@
+use extism_pdk::*;
+use fluentci_pdk::dag;
+
+#[plugin_fn]
+pub fn deploy(args: String) -> FnResult<String> {
+    let stdout = dag()
+        .pipeline("deploy")?
+        .pkgx()?
+        .with_exec(vec![
+            "pkgx",
+            "install",
+            "node",
+            "bun",
+            "classic.yarnpkg.com",
+        ])?
+        .with_exec(vec!["yarn", "install"])?
+        .with_exec(vec!["bunx", "wrangler", "deploy", &args])?
+        .stdout()?;
+    Ok(stdout)
+}
+
+#[plugin_fn]
+pub fn pages_deploy(args: String) -> FnResult<String> {
+    let stdout = dag()
+        .pipeline("pages_deploy")?
+        .pkgx()?
+        .with_exec(vec![
+            "pkgx",
+            "install",
+            "node",
+            "bun",
+            "classic.yarnpkg.com",
+        ])?
+        .with_exec(vec!["bunx", "wrangler", "pages", "deploy", &args])?
+        .stdout()?;
+    Ok(stdout)
+}
