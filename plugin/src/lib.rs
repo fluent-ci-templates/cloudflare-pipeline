a3b0f17a4c90cc080d133ec9f5a11c1c00fdc10b
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2024-04-08 05:45:18 +0000

    [plugin] use fluentci-pdk v0.1.9

--- plugin/src/lib.rs
+++ plugin/src/lib.rs
@@ -6,13 +6,7 @@ pub fn deploy(args: String) -> FnResult<String> {
     let stdout = dag()
         .pipeline("deploy")?
         .pkgx()?
-        .with_exec(vec![
-            "pkgx",
-            "install",
-            "node",
-            "bun",
-            "classic.yarnpkg.com",
-        ])?
+        .with_packages(vec!["node", "bun", "classic.yarnpkg.com"])?
         .with_exec(vec!["yarn", "install"])?
         .with_exec(vec!["bunx", "wrangler", "deploy", &args])?
         .stdout()?;
@@ -24,13 +18,7 @@ pub fn pages_deploy(args: String) -> FnResult<String> {
     let stdout = dag()
         .pipeline("pages_deploy")?
         .pkgx()?
-        .with_exec(vec![
-            "pkgx",
-            "install",
-            "node",
-            "bun",
-            "classic.yarnpkg.com",
-        ])?
+        .with_packages(vec!["node", "bun", "classic.yarnpkg.com"])?
         .with_exec(vec!["bunx", "wrangler", "pages", "deploy", &args])?
         .stdout()?;
     Ok(stdout)
